6e5254a8dd833bbfc5b3e0c89ef2819f3d1a088c
lefpap <lolopap21@gmail.com>
2025-03-25 21:43:31 +0200

    Adds docs and removes the context complex feature

--- src/test/java/org/github/lefpap/sample/ContextualTestCommand.java
+++
@@ -1,8 +0,0 @@
-package org.github.lefpap.sample;
-
-import org.github.lefpap.mdtr.message.Command;
-import org.github.lefpap.mdtr.message.CommandContext;
-
-public record ContextualTestCommand() implements Command {
-    public record ContextualTestCommandCtx() implements CommandContext<ContextualTestCommand> {}
-}
