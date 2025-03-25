7d2cd54064da2896d8193d33707857fd3bbfe934
lefpap <lolopap21@gmail.com>
2025-03-25 21:43:31 +0200

    Adds docs and removes the context complex feature

--- src/main/java/org/github/lefpap/mdtr/registry/HandlerRegistry.java
+++ src/main/java/org/github/lefpap/mdtr/registry/HandlerRegistry.java
@@ -1,23 +1,15 @@
 package org.github.lefpap.mdtr.registry;
 
 import org.github.lefpap.mdtr.handler.CommandHandler;
-import org.github.lefpap.mdtr.handler.ContextualCommandHandler;
-import org.github.lefpap.mdtr.handler.ContextualQueryHandler;
 import org.github.lefpap.mdtr.handler.QueryHandler;
 import org.github.lefpap.mdtr.message.Command;
-import org.github.lefpap.mdtr.message.CommandContext;
 import org.github.lefpap.mdtr.message.Query;
-import org.github.lefpap.mdtr.message.QueryContext;
 
 import java.util.Optional;
 
 
 /**
  * A registry for registering and retrieving command and query handlers.
- * <p>
- * This interface provides overloaded methods for registering both contextual and non-contextual
- * handlers, as well as lookup methods to retrieve the appropriate handler for a given command or query.
- * </p>
  */
 public interface HandlerRegistry {
 
@@ -34,17 +26,6 @@ public interface HandlerRegistry {
     <C extends Command, R>
     void registerCommandHandler(Class<C> commandType, CommandHandler<C, R> handler);
 
-    /**
-     * Registers a contextual command handler for a specific command type.
-     *
-     * @param <C>         the type of command handled
-     * @param <CTX>       the type of context required by the command handler
-     * @param <R>         the type of result produced by the handler
-     * @param commandType the class of the command to be handled
-     * @param handler     the contextual command handler that will process the command using additional context
-     */
-    <C extends Command, CTX extends CommandContext<C>, R>
-    void registerCommandHandler(Class<C> commandType, ContextualCommandHandler<C, CTX, R> handler);
 
     /**
      * Registers a query handler for a specific query type.
@@ -56,18 +37,6 @@ public interface HandlerRegistry {
      */
     <Q extends Query, R>
     void registerQueryHandler(Class<Q> queryType, QueryHandler<Q, R> handler);
-
-    /**
-     * Registers a contextual query handler for a specific query type.
-     *
-     * @param <Q>       the type of query handled
-     * @param <CTX>     the type of context required by the query handler
-     * @param <R>       the type of result produced by the handler
-     * @param queryType the class of the query to be handled
-     * @param handler   the contextual query handler that will process the query using additional context
-     */
-    <Q extends Query, CTX extends QueryContext<Q>, R>
-    void registerQueryHandler(Class<Q> queryType, ContextualQueryHandler<Q, CTX, R> handler);
 
     // ---------------------- Lookup Methods ----------------------
 
@@ -81,16 +50,6 @@ public interface HandlerRegistry {
      */
     <C extends Command, R> Optional<CommandHandler<C, R>> getCommandHandler(C command);
 
-    /**
-     * Retrieves a registered contextual command handler for the given command.
-     *
-     * @param <C>     the type of command
-     * @param <CTX>   the type of context required by the handler
-     * @param <R>     the type of result expected from the handler
-     * @param command the command for which a contextual handler is sought
-     * @return an {@link Optional} containing the matching contextual command handler if one is registered, or an empty Optional otherwise
-     */
-    <C extends Command, CTX extends CommandContext<C>, R> Optional<ContextualCommandHandler<C, CTX, R>> getContextualCommandHandler(C command);
 
     /**
      * Retrieves a registered query handler for the given query.
@@ -101,17 +60,5 @@ public interface HandlerRegistry {
      * @return an {@link Optional} containing the matching query handler if one is registered, or an empty Optional otherwise
      */
     <Q extends Query, R> Optional<QueryHandler<Q, R>> getQueryHandler(Q query);
-
-    /**
-     * Retrieves a registered contextual query handler for the given query.
-     *
-     * @param <Q>   the type of query
-     * @param <CTX> the type of context required by the handler
-     * @param <R>   the type of result expected from the handler
-     * @param query the query for which a contextual handler is sought
-     * @return an {@link Optional} containing the matching contextual query handler if one is registered, or an empty Optional otherwise
-     */
-    <Q extends Query, CTX extends QueryContext<Q>, R> Optional<ContextualQueryHandler<Q, CTX, R>> getContextualQueryHandler(Q query);
-
 }
 
--- src/main/java/org/github/lefpap/mdtr/registry/InMemoryHandlerRegistry.java
+++ src/main/java/org/github/lefpap/mdtr/registry/InMemoryHandlerRegistry.java
@@ -1,13 +1,9 @@
 package org.github.lefpap.mdtr.registry;
 
 import org.github.lefpap.mdtr.handler.CommandHandler;
-import org.github.lefpap.mdtr.handler.ContextualCommandHandler;
-import org.github.lefpap.mdtr.handler.ContextualQueryHandler;
 import org.github.lefpap.mdtr.handler.QueryHandler;
 import org.github.lefpap.mdtr.message.Command;
-import org.github.lefpap.mdtr.message.CommandContext;
 import org.github.lefpap.mdtr.message.Query;
-import org.github.lefpap.mdtr.message.QueryContext;
 
 import java.util.Map;
 import java.util.Optional;
@@ -28,13 +24,10 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class InMemoryHandlerRegistry implements HandlerRegistry {
 
-    // Maps for non-contextual handlers.
+    // Maps for handlers.
     private final Map<Class<? extends Command>, CommandHandler<?, ?>> commandHandlers = new ConcurrentHashMap<>();
     private final Map<Class<? extends Query>, QueryHandler<?, ?>> queryHandlers = new ConcurrentHashMap<>();
 
-    // Maps for contextual handlers.
-    private final Map<Class<? extends Command>, ContextualCommandHandler<?, ?, ?>> contextualCommandHandlers = new ConcurrentHashMap<>();
-    private final Map<Class<? extends Query>, ContextualQueryHandler<?, ?, ?>> contextualQueryHandlers = new ConcurrentHashMap<>();
 
     // Overloaded registration for non-contextual command handlers.
     @Override
@@ -42,24 +35,10 @@ public class InMemoryHandlerRegistry implements HandlerRegistry {
         commandHandlers.put(commandType, handler);
     }
 
-    // Overloaded registration for contextual command handlers.
-    @Override
-    public <C extends Command, CTX extends CommandContext<C>, R>
-    void registerCommandHandler(Class<C> commandType, ContextualCommandHandler<C, CTX, R> handler) {
-        contextualCommandHandlers.put(commandType, handler);
-    }
-
     // Overloaded registration for non-contextual query handlers.
     @Override
     public <Q extends Query, R> void registerQueryHandler(Class<Q> queryType, QueryHandler<Q, R> handler) {
         queryHandlers.put(queryType, handler);
-    }
-
-    // Overloaded registration for contextual query handlers.
-    @Override
-    public <Q extends Query, CTX extends QueryContext<Q>, R>
-    void registerQueryHandler(Class<Q> queryType, ContextualQueryHandler<Q, CTX, R> handler) {
-        contextualQueryHandlers.put(queryType, handler);
     }
 
     // Lookup for non-contextual command handlers.
@@ -69,26 +48,10 @@ public class InMemoryHandlerRegistry implements HandlerRegistry {
         return Optional.ofNullable((CommandHandler<C, R>) commandHandlers.get(command.getClass()));
     }
 
-    // Lookup for contextual command handlers.
-    @SuppressWarnings("unchecked")
-    @Override
-    public <C extends Command, CTX extends CommandContext<C>, R>
-    Optional<ContextualCommandHandler<C, CTX, R>> getContextualCommandHandler(C command) {
-        return Optional.ofNullable((ContextualCommandHandler<C, CTX, R>) contextualCommandHandlers.get(command.getClass()));
-    }
-
     // Lookup for non-contextual query handlers.
     @SuppressWarnings("unchecked")
     @Override
     public <Q extends Query, R> Optional<QueryHandler<Q, R>> getQueryHandler(Q query) {
         return Optional.ofNullable((QueryHandler<Q, R>) queryHandlers.get(query.getClass()));
-    }
-
-    // Lookup for contextual query handlers.
-    @SuppressWarnings("unchecked")
-    @Override
-    public <Q extends Query, CTX extends QueryContext<Q>, R>
-    Optional<ContextualQueryHandler<Q, CTX, R>> getContextualQueryHandler(Q query) {
-        return Optional.ofNullable((ContextualQueryHandler<Q, CTX, R>) contextualQueryHandlers.get(query.getClass()));
     }
 }
